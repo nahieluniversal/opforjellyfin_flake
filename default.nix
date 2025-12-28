28e2a785b0088d27769c186879c8643615708201
nahieluniversal <82026760+nahieluniversal@users.noreply.github.com>
2025-12-28 21:40:42 +0100

    Added default.nix package file

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ lib, stdenv, buildGoModule, fetchFromGitHub, git }:
+
+buildGoModule rec {
+  pname = "opforjellyfin";
+  version = "1.0.1_nixosalpha1";
+
+  src = fetchFromGitHub {
+    owner = "tissla";
+    repo = "opforjellyfin";
+    rev = "d6133d89836c9c727438794ab634f6a2b1184540";
+    hash = "sha256-lSB+F7heenXEmr6T+PKTRC9ZLEPGMcG5nEtVTzJUe+A=";
+  };
+
+  vendorHash = "sha256-PL42t4SywbXmpPtetau03AsTHAGmhOrajsSyF4LJwUU=";
+  nativeBuildInputs = [ git ];
+  propagatedBuildInputs = [ git ];
+
+  meta = with lib; {
+    description = "CLI to automate download and organisation of One Pace episodes for Jellyfin";
+    homepage = "https://github.com/tissla/opforjellyfin";
+    license = licenses.mit;
+    platforms = platforms.linux;
+    maintainers = [ nahieluniversal ];
+  };
+}
